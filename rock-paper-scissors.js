3a7889112941c8bc6bee60dc6b474ed30c87a248
Victor <victor.carranz@hotmail.com>
2019-02-25 20:54:03 -0600

    Done :squirrel:

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,24 +1,67 @@
+function you(){
+    let userSelect = prompt("Rock, Paper or Scissors?");
+    userSelect.toLowerCase();
+    return userSelect;
+}
+
+
 function computerPlay(){
  let hand = Math.random();
- console.log(hand);
  if(hand <= 0.3333){
-     console.log("paper");
-     return "Paper";
+     return "paper";
  }else if(hand >0.3333 && hand <= 0.6666){
-    console.log("Scissors");
-    return "Scissors"
+    return "scissors"
  }else{
-     console.log("rock");
      return "rock"
  }
 }
 
 function playRound(playerSeleccion, computerSelection){
+    
+    if(Object.is(playerSeleccion, computerSelection)){
+        console.log("Holy shit it's a Draw")
+    }else if(playerSeleccion=="paper" && computerSelection=="scissors"){
+        console.log("you lose! Scissors beats paper")
+        return "c";
+    }else if(playerSeleccion=="scissors" && computerSelection=="rock"){
+        console.log("you lose! rock beats scissors")
+        return "c";
+    }else if(playerSeleccion=="rock" && computerSelection=="paper"){
+        console.log("you lose! paper beats rock")
+        return "c";
+    }
+    else if(playerSeleccion=="paper" && computerSelection=="rock"){
+        console.log("you win! paper beats rock")
+        return "u";
+    }else if(playerSeleccion=="scissors" && computerSelection=="paper"){
+        console.log("you win! scissors beats paper")
+        return "u";
+    }else if(playerSeleccion=="rock" && computerSelection=="scissors"){
+        console.log("you win! rock beats scissors")
+        return "u";
+    }else{
+        console.log("do you wrote it right?... try again")
+    }
 
-}
+    }
 
 function game (){
-
+    let user = 0;
+    let comp = 0;
+    for(let i=0; i<5; i++){
+        let value = playRound(you(),computerPlay());
+        if(value=="c"){
+            comp++;
+        }else if(value =="u"){
+            user++;
+        }
+        console.log(`Score: You = ${user} Computer = ${comp}`)
+    }
+    if(user>comp){
+        console.log("well done, You Win!")
+    }else{
+        console.log("I won, sucker!");
+    }
 }
 
-console.log("esta wea es un: "+computerPlay());
+game();
